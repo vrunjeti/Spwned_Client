89e5c922ea554185fd274e7f350dd011333299a4
Serin Yoon <syoon17@illinois.edu>
2015-05-11 10:10:30 +0000

    msg

--- app/scripts/controllers/messages.js
+++ app/scripts/controllers/messages.js
@@ -8,6 +8,53 @@
  * Controller of the spwnedApp
  */
 angular.module('spwnedApp')
-  .controller('MessagesCtrl', function () {
+  .controller('MessagesCtrl', ['Users', 'Messages', '$scope', '$http', '$window', '$location', '$routeParams', function(Users, Messages, $scope, $http, $window, $location, $routeParams) {
 
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
   });
+
+  $scope.user = $window.sessionStorage.userId;
+  $scope.isAdmin = Users.getAllUsers().success(function(data){
+  $scope.users = [];
+  	data.data.forEach(function(user){
+  		var index = user.games.indexOf($routeParams.game_id);
+  		if(index > -1) {
+  			user.name = user.first_name + ' ' + user.last_name;
+  			$scope.users.push(user);
+  		}
+  	});
+  });
+
+ 	Messages.getMessages($routeParams.gameid, $routeParams.userid)
+ 		.success(function(data){
+ 			$scope.msgs = data.data;
+ 			data.data.forEach(function(msg){
+ 				Users.getUserAccount(msg.sender_id)
+ 					.success(function(data){
+ 						msg.sender = data.data.first_name + ' ' + data.data.last_name;
+ 					});
+ 				Users.getUserAccount(msg.recipient_id)
+ 					.success(function(data){
+ 						msg.recipient = data.data.first_name + ' ' + data.data.last_name;
+ 					});
+ 			});
+ 			
+ 		});
+
+	$scope.reply = function(recipient_id, body) {
+		var msg = {
+			game_id : $routeParams.gameid,
+			sender_id : $routeParams.userid,
+			recipient : recipient_id,
+			body : body
+		}
+
+		Messages.sendMessage($routeParams.gameid, $routeParams.userid, msg)
+			.success(function(data){
+				$location.path('/messages/g/' + $routeParams.gameid + '/u/' + $routeParams.userid);
+			});
+	}
+
+}]);
